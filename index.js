a1ca656e134a06cf4fea5d3d5ba78347c40ba549
Tony Lechner <tony.lechner@gmail.com>
2016-11-24 20:01:26 +0000

    Added html

--- index.js
+++ index.js
@@ -1,39 +1,76 @@
-var rest = require('restler');
-var moment = require('moment');
+<!DOCTYPE html>
+<html>
+<head>
+<title>TEST Ratting Capitals</title>
+<style>
+    body {
+        margin: 0 auto;
+        font-family: Helvetica, Arial, sans-serif;
+        text-align: center;
+        font-size: 24pt;
+    }
 
-var ZKILL_BASE_URL = 'https://zkillboard.com';
-var ZKILL_RESOURCE = '/api/losses/alliance/498125261/group/547,513,902,30,659,883/no-items/no-attackers/limit/1';
+    a {
+      text-decoration: none;
+    }
 
-function resultObject(daysSince = 0, url) {
-  this.daysSince = daysSince;
-  this.url = url;
-};
+    a:hover {
+      text-decoration: underline;
+    }
 
-function parseZKill(zKill) {
-  var result;
+    .container strong {
+			color: #900;
+			font-size: 36pt;
+			font-weight: bold;
+    }
 
-  if (!zKill || !zKill.killTime || !zKill.killID) {
-    result = new resultObject(-1);
-    return result;
-  }
+    .container p.callout {
+      font-size: 12pt;
+    }
 
-  var now = moment(new Date().getTime());
-  var killDate = moment(zKill.killTime);
-  var daysSince = now.diff(killDate, 'days');
+    .container p.shame {
+      font-size: 8pt;
+      text-decoration: none;
+      position: absolute;
+      bottom: 0;
+      text-align: center;
+      margin: 1em auto;
+      width: 100%;
+    }
+</style>
+</head>
+<body>
+<div class="container">
+</div>
 
-  var url = ZKILL_BASE_URL + '/kill/' + zKill.killID;
+<script type="text/javascript" src="//code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+  $(function() {
+    $.getJSON('/data.json',
+      function(data) {
+        if (!data || typeof(data) !== 'object' || data.hasOwnProperty('daysSince') === false) {
+          return $('.container').html('<p>RIP. An error has occurred.</p>');
+        }
 
-  return new resultObject(daysSince, url);
-};
+        if (data.daysSince < 0) {
+          return $('.container').html('<p>You\'ve created a time paradox.</p>');
+        }
 
-rest.get(ZKILL_BASE_URL + ZKILL_RESOURCE).on('complete', function (result) {
-  if (result instanceof Error) return;
-  if (Array.isArray(result) === false) return;
-  if (result.length === 0 || !result[0]) return;
+        var BODY_FORMAT = [
+          '<p>It has been <strong>{{DAYS_SINCE}}</strong> days since a TEST ratting capital died.</p>',
+          '<p class="callout"><a href="{{ZKILL_URL}}">{{ZKILL_URL}}</a></p>',
+          '<p class="shame"><a href="https://zkillboard.com/alliance/498125261/losses/group/547,513,902,941,30,659,883/">Wall of shame</a></p>'
+        ]
+        .join('\n')
 
-  console.log(
-    JSON.stringify(
-      parseZKill(result[0])
-    )
-  );
-});
+        var result = BODY_FORMAT
+        .replace(new RegExp('{{DAYS_SINCE}}', 'g'), data.daysSince)
+        .replace(new RegExp('{{ZKILL_URL}}', 'g'), data.url);
+
+        $('.container').html(result);
+      }
+    );
+  });
+</script>
+</body>
+</html>
