1c34218789757586b34f4e901632d2d0f8039995
Tony Lechner <tony@tony-lechner.com>
2017-07-22 22:51:26 -0500

    Add defines

---
+++ daemon/src/defines.py
@@ -0,0 +1,40 @@
+TRACKING_LABEL_SHIP_IDS = {
+    """Ship IDs to associate with a particular tracking label
+    """
+        'ratting_capital': [
+            # CARRIER
+            23757, # archon
+            23911, # thanatos
+            24483, # nidhogger
+            23915, # chimera
+
+            # SUPERCARRIER
+            23919, # aeon
+            23913, # nyx
+            22852, # hel
+            23917, # wyvern
+            3514,  # revenant
+            42125, # vendetta
+
+            # MINING
+            28606, # orca
+            28352, # rorqual
+            33687, # rorqual ore development edition
+
+            # FREIGHTER
+            20183, # providence
+            20187, # obelisk
+            20189, # fenrir
+            20185, # charon
+            34328, # bowhead
+
+            # JUMP FREIGHTER
+            28850, # ark
+            28848, # anshar
+            28846, # nomad
+            28844, # rhea
+        ],
+        'vni': [
+            17843, # vexor navy issue
+        ],
+}
