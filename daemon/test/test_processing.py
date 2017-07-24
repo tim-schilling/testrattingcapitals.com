9578e8d7ef43ffb9c668687ac418ce63a64094dc
Tony Lechner <tony@tony-lechner.com>
2017-07-24 00:29:35 -0500

    Add edge case to processing test

--- daemon/test/test_processing.py
+++ daemon/test/test_processing.py
@@ -5,12 +5,20 @@ def mock_processor_ok(zkill):
     return 'UNIT_TEST'
 
 
+def mock_processor_ok_2(zkill):
+    return 'UNIT_TEST_2'
+
+
 def mock_processor_none(zkill):
     return None
 
 
 def test_process(monkeypatch):
-    monkeypatch.setattr(unit, 'PROCESSORS', [mock_processor_ok, mock_processor_none])
+    monkeypatch.setattr(unit, 'PROCESSORS', [
+        mock_processor_ok,
+        mock_processor_ok_2,
+        mock_processor_none,
+    ])
     test_input = {
         'package': {
             'killID': 1,
@@ -30,5 +38,6 @@ def test_process(monkeypatch):
     result = unit.process(test_input)
 
     assert isinstance(result, set)
-    assert len(result) == 1
+    assert len(result) == 2
     assert 'UNIT_TEST' in result
+    assert 'UNIT_TEST_2' in result
