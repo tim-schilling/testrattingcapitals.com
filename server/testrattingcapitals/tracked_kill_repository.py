70c77c2562a6412f2e0ac9460fd94ee11c4b20e5
Tony Lechner <tony@tony-lechner.com>
2017-07-26 23:01:00 -0500

    Resolve typo preventing tracked_kill entities from persisting

--- server/testrattingcapitals/tracked_kill_repository.py
+++ server/testrattingcapitals/tracked_kill_repository.py
@@ -27,8 +27,8 @@ def add(tracked_kill):
         if existing == 0:
             logger.debug('{}-{} not in persistent storage. Writing.'.format(tracked_kill.kill_id, tracked_kill.kill_tracking_label))
             # get system, ship name from lookup tables
-            tracked_kill.ship_name = context.session.query(EveItem.Name).filter(EveItem.id == tracked_kill.ship_id).one()
-            tracked_kill.system_name = context.session.query(EveSolarSystem.Name).filter(EveSolarSystem.id == tracked_kill.system_id).one()
+            tracked_kill.ship_name = context.session.query(EveItem.name).filter(EveItem.id == tracked_kill.ship_id).one()
+            tracked_kill.system_name = context.session.query(EveSolarSystem.name).filter(EveSolarSystem.id == tracked_kill.system_id).one()
             context.session.add(tracked_kill)
             context.session.commit()
         else:
