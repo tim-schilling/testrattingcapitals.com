85ecfb7712446a959a023a3b66fe5af3274aa67f
Tony Lechner <tony@tony-lechner.com>
2017-07-28 02:01:03 -0500

    Add redis singleton

---
+++ server/test/test_redis.py
@@ -0,0 +1,43 @@
+"""
+  Copyright (c) 2016-2017 Tony Lechner and contributors
+
+  testrattingcapitals.com is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  testrattingcapitals.com is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with testrattingcapitals.com.  If not, see
+  <http://www.gnu.org/licenses/>.
+"""
+
+from testrattingcapitals import redis as unit
+
+
+class MockStrictRedis(object):
+    def __init__(self, url):
+        self.url = url
+
+    @staticmethod
+    def from_url(url):
+        return MockStrictRedis('unit-test-url')
+
+
+def setup_module(module):
+    module.unit._singleton = None
+
+
+def teardown_module(module):
+    module.unit._singleton = None
+
+
+def test_singleton_behavior(monkeypatch):
+    monkeypatch.setattr(unit, 'StrictRedis', MockStrictRedis)
+    first_ref = unit.get_redis_singleton()
+    second_ref = unit.get_redis_singleton()
+    assert first_ref is second_ref
---
+++ server/testrattingcapitals/redis.py
@@ -0,0 +1,36 @@
+"""
+  Copyright (c) 2016-2017 Tony Lechner and contributors
+
+  testrattingcapitals.com is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  testrattingcapitals.com is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with testrattingcapitals.com.  If not, see
+  <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+import os
+from redis import StrictRedis
+
+DEFAULT_REDIS_CONNECTION_STRING = 'redis://localhost:6379/0'
+
+logger = logging.getLogger('testrattingcapitals')
+
+_singleton = None
+
+
+def get_redis_singleton():
+    global _singleton
+    if not _singleton:
+        _singleton = StrictRedis.from_url(
+            os.getenv('REDIS_CONNECTION_STRING', DEFAULT_REDIS_CONNECTION_STRING)
+        )
+    return _singleton
