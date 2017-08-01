0e658c8d4aada3925acd0fce08aed6c0cc3e18f8
Tony Lechner <tony@tony-lechner.com>
2017-07-31 23:09:25 -0500

    Skeleton flask/uwgi

--- server/testrattingcapitals/bin/__init__.py
+++ server/testrattingcapitals/bin/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
   Copyright (c) 2016-2017 Tony Lechner and contributors
 
@@ -16,27 +15,3 @@
   along with testrattingcapitals.com.  If not, see
   <http://www.gnu.org/licenses/>.
 """
-
-from flask import Flask
-import logging
-import os
-import signal
-import sys
-
-logger = logging.getLogger('testrattingcapitals')
-
-app = Flask(__name__)
-
-def configure_logging():
-    logger.setLevel(int(os.getenv('LOG_LEVEL', logging.DEBUG)))
-    stdio = logging.StreamHandler(sys.stdout)
-    stdio.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(stdio)
-
-configure_logging()
-    
-logger.info('testrattingcapitals api started')
-
-@app.route('/')
-def health():
-    return 'OK'
